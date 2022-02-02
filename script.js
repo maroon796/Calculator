b15cd35e158867f6e43823cc119db9180048b517
Grygory Ksheminsky <maroon785@gmail.com>
2022-02-03 01:05:43 +0200

    implementing multiple numbers in output

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 'use strict';
 
-const firstNum = 0;
-const secondNum = 0;
-const operationType = 0;
-const result = document.querySelector('.el--1');
-const division = document.querySelector('.el--5');
-const one = document.querySelector('.el--10');
+const numElements = document.querySelectorAll('.num');
+const output = document.querySelector('.el--1');
 
-const plus = document.querySelector('.el--16');
-const equal = document.querySelector('.el--17');
-
-one.addEventListener('click', function () {
-  result.textContent = '1';
-  one.textContent = '1';
-});
-
-// Operations
-equal.addEventListener('click', function () {});
-
-plus.addEventListener('click', function (params) {});
-
-// Result
-division.addEventListener('click', function (x, y) {
-  result.textContent = x / y;
+numElements.forEach(function (number) {
+  number.addEventListener('click', function () {
+    if (output.textContent == 0) {
+      output.textContent = number.textContent;
+    } else if (output.textContent != 0) {
+      output.textContent = output.textContent + number.textContent;
+    }
+  });
 });
