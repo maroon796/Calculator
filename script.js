01c634a246f9cc5b7746e474790ac099b38b6939
Grygory Ksheminsky <maroon785@gmail.com>
2022-02-03 02:21:33 +0200

    implementing multiplying

--- script.js
+++ script.js
@@ -2,13 +2,59 @@
 
 const numElements = document.querySelectorAll('.num');
 const output = document.querySelector('.el--1');
+let firstNum;
+let secondNum;
+let operation;
+const equal = document.querySelector('.equal');
+const multy = document.querySelector('.mult');
 
 numElements.forEach(function (number) {
   number.addEventListener('click', function () {
-    if (output.textContent == 0) {
+    if (output.textContent == 0 && !operation) {
       output.textContent = number.textContent;
-    } else if (output.textContent != 0) {
+      firstNum = number.textContent;
+      console.log(firstNum);
+    } else if (output.textContent != 0 && !operation) {
       output.textContent = output.textContent + number.textContent;
+      firstNum = output.textContent;
+      console.log(firstNum);
+    } else if (operation) {
+      output.textContent = number.textContent;
+      secondNum = number.textContent;
+      console.log(secondNum);
     }
+    // else if (output.textContent == 0 && operation) {
+    //   output.textContent = number.textContent;
+    //   secondNum = number.textContent;
+    //   console.log(secondNum);
+    // } else if (output.textContent != 0 && operation) {
+    //   console.log('Hey!');
+    //   output.textContent = output.textContent + number.textContent;
+    //   secondNum = output.textContent;
+    //   console.log(secondNum);
+    // }
   });
+});
+
+multy.addEventListener('click', function () {
+  console.log('*');
+  operation = 'mult';
+});
+
+const mult = (a, b) => Number(a) * Number(b);
+const divide = (a, b) => a / b;
+const plus = (a, b) => a + b;
+const minus = (a, b) => a - b;
+
+equal.addEventListener('click', function () {
+  if (operation === 'mult') {
+    console.log('Finish');
+    output.textContent = mult(firstNum, secondNum);
+  } else if (operation === 'div') {
+    output.textContent = divide(firstNum, secondNum);
+  } else if (operation === 'plus') {
+    output.textContent = plus(firstNum, secondNum);
+  } else if (operation === 'minus') {
+    output.textContent = minus(firstNum, secondNum);
+  }
 });
